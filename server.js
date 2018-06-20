b080cc653f10a5e466ca5ec2ffcbf67f6922787a
Preet Singh <Preet.singh29@gmail.com>
2018-06-20 11:50:25 -0400

    eric fixed it

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const app = express();
 // and fallback to 4567
 const PORT = process.env.PORT || 4567;
 
+app.use('/static', express.static('build/static'));
+
 // In production, any request that doesn't match a previous route
 // should send the front-end application, which will handle the route.
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
   app.get("/*", function (request, response) {
     response.sendFile(path.join(__dirname, "build", "index.html"));
   });
-}
+// }
 
 // Start the web server listening on the provided port.
 app.listen(PORT, () => {
