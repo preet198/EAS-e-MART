05dcb13d501a20858c17f1eb97abf3a4e7c33599
andre <a.e.pykhantsev@gmail.com>
2018-06-20 12:56:02 -0400

    filled out schema and seed files

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,38 @@
-hello world
+DROP DATABASE IF EXISTS eas_e_mart;
+CREATE DATABASE eas_e_mart;
+
+\c eas_e_mart;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS items;
+
+
+
+CREATE table users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL unique,
+  password_digest VARCHAR NOT NULL,
+  phone_number VARCHAR(10),
+  email TEXT NOT NULL,
+  location TEXT NOT NULL,
+  first_name TEXT,
+  last_name TEXT,
+)
+
+CREATE table categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(20),
+  img_url TEXT,
+)
+
+CREATE table items (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  category_id INTEGER REFERENCES categories(id),
+  name VARCHAR (50),
+  description TEXT,
+  price VARCHAR (20),
+  condition VARCHAR(10),
+  quantity INTEGER,
+);
