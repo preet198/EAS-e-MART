26f5670cbe76ac1c78008dc0610169c4c6d30a79
andre <a.e.pykhantsev@gmail.com>
2018-06-21 12:39:30 -0400

    homepage component

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from '../HomePage';
 import "./style.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <Router>
+      <div>
+      <div className="App">
+        <img src="https://farm2.staticflickr.com/1810/42939870751_33b6a555e2_b.jpg" className="logo"/>
+        <nav>
+          <Link to="/">Home Page</Link>
+          <br></br>
+          <Link to="/login">LogIn *register in logIn modal</Link>
+        </nav>
+          <Route path="/" exact component = {HomePage} />
+        </div>
+      </div>
+      </Router>
+    );
   }
 }
 
